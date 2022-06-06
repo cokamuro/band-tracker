9cbdb4fedb89a486880693884908df827241cc01
Chris Okamuro <cokamuro@gmail.com>
2022-06-06 10:58:39 -0400

    added some of getWeather and API keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,5 +52,24 @@ function getWeather(city,dateEvent,containerElement){
     //if dateEvent<=(today's date+7 days)
         //get lat/long from city, state, country?
         //if data, add image of 1x weather icon, setting alt text to text description, and add high temperature element
+
+
+            //lookup data based on city name
+            //https://openweathermap.org/api/geocoding-api
+        
+            //get lat, long off of response, test for bad response - limit to 1 city returned
+            var params = "q=" + city + "&limit=1&appid=" + apiOpenWeather
+            fetch("https://api.openweathermap.org/geo/1.0/direct?" + params)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    if (data.length > 0) {
+                        var item = data[0]
+                        // make the next call to get the weather
+                        // call getWeatherByGCS, passing city name, latitude, and longitude
+                        getWeatherByGCS(item.name, item.lat, item.lon);
+                    }
+                });
 }
 
